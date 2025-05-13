f525b5cb46fd780bd8c65015e4de2d1cedf2aaec
marciatorresg <marciatorresg@gmail.com>
2025-05-12 23:29:01 -0500

    Se modificó formato y pertaña de Camaras.

--- src/app/cameras/cameras.page.spec.ts
+++ src/app/cameras/cameras.page.spec.ts
@@ -1,5 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CamerasPage } from './cameras.page';
+import { Component } from '@angular/core';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: 'example.component.html',
+  styleUrls: ['example.component.css'],
+  imports: [IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar],
+})
+export class ExampleComponent {}
 
 describe('CamerasPage', () => {
   let component: CamerasPage;
