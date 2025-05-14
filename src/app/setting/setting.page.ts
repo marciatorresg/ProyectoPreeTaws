84d71c1c3cc0c83140efaacf665de2785083aaf8
marciatorresg <marciatorresg@gmail.com>
2025-05-14 00:08:03 -0500

    Se terminó la página Setting.

--- src/app/setting/setting.page.ts
+++ src/app/setting/setting.page.ts
@@ -3,16 +3,50 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonRange,
+  IonText,
+  IonToggle,
+} from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { personCircle, personCircleOutline, sunny, sunnyOutline } from 'ionicons/icons';
+
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.page.html',
   styleUrls: ['./setting.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonBackButton,
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonRange,
+    IonText,
+    IonTitle,
+    IonToggle,
+    IonToolbar,IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SettingPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+        addIcons({ personCircle, personCircleOutline, sunny, sunnyOutline });
+
+  }
 
   ngOnInit() {
   }
